534799b1d1e2988da4c8ec40c7e3b9a217b3d87b
sheikrizwan2006 <sheikrizwan343@gmail.com>
2026-03-20 09:26:49 +0530

    UC4: Maintain ordered train consist using LinkedList

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -18,9 +18,45 @@ public class TrainApp {
         System.out.println("After adding bogies:");
         System.out.println("Total bogies: " + consist.size());
 
-        // UC3: Display all bogies
         System.out.println("Train consist:");
         for(String bogie : consist) {
+            System.out.println(bogie);
+        }
+        // UC3: Track Unique Bogie IDs using HashSet
+
+        Set<String> bogieIds = new HashSet<>();
+
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101"); // duplicate
+        bogieIds.add("BG102"); // duplicate
+
+        System.out.println("Unique Bogie IDs:");
+        for(String id : bogieIds) {
+            System.out.println(id);
+        }
+        // UC4: Maintain ordered train consist using LinkedList
+
+        LinkedList<String> train = new LinkedList<>();
+
+// Add bogies
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
+
+// Insert Pantry Car at position 2
+        train.add(2, "Pantry");
+
+// Remove first and last bogie
+        train.removeFirst();
+        train.removeLast();
+
+// Display final train consist
+        System.out.println("Final Train Consist:");
+        for(String bogie : train) {
             System.out.println(bogie);
         }
     }
