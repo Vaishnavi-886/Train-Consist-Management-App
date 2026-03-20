b0a433750f3b162915cdbc6a45f0a30e145234ba
sheikrizwan2006 <sheikrizwan343@gmail.com>
2026-03-20 09:09:08 +0530

    UC2: Add Passenger Bogies to Train

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -2,12 +2,20 @@ import java.util.*;
 
 public class TrainApp {
     public static void main(String[] args) {
-
+        // UC1: Initialize Train
         System.out.println("=== Train Consist Management App ===");
 
         List<String> consist = new ArrayList<>();
 
         System.out.println("Train consist initialized.");
         System.out.println("Initial bogie count: " + consist.size());
+
+        // UC2: Add Passenger Bogies
+        consist.add("Sleeper");
+        consist.add("AC Chair");
+        consist.add("First Class");
+
+        System.out.println("After adding bogies:");
+        System.out.println("Total bogies: " + consist.size());
     }
 }
