dc463eefbe840b3eb46d6d249ee800aa806e0f7b
kvsaijagadeesh2007 <saijagadeeshkolipakula@gmail.com>
2026-04-07 09:07:47 +0530

    UC15: Average Capacity

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -82,4 +82,63 @@ public class TrainConsistManagementApp {
         sortByName(list);
         uniqueTypes(list);
     }
+    // UC15 - Average Capacity
+    static void averageCapacity(List<Bogie> list) {
+        double avg = list.stream()
+                .mapToInt(b -> b.capacity)
+                .average()
+                .orElse(0);
+
+        System.out.println("\nAverage Capacity: " + avg);
+    }
+
+    // UC16 - Get All Bogie Names
+    static void getAllBogieNames(List<Bogie> list) {
+        List<String> names = list.stream()
+                .map(b -> b.name)
+                .collect(Collectors.toList());
+
+        System.out.println("\nBogie Names: " + names);
+    }
+
+    // UC17 - Check High Capacity (>70)
+    static void checkHighCapacity(List<Bogie> list) {
+        boolean exists = list.stream()
+                .anyMatch(b -> b.capacity > 70);
+
+        System.out.println("\nAny capacity > 70: " + exists);
+    }
+
+    // UC18 - Partition Passenger vs Goods
+    static void partitionBogies(List<Bogie> list) {
+        Map<Boolean, List<Bogie>> map =
+                list.stream()
+                        .collect(Collectors.partitioningBy(
+                                b -> b.type.equalsIgnoreCase("Passenger")));
+
+        System.out.println("\nPassenger Bogies:");
+        map.get(true).forEach(b -> System.out.println(b.name));
+
+        System.out.println("\nGoods Bogies:");
+        map.get(false).forEach(b -> System.out.println(b.name));
+    }
+
+    // UC19 - Convert to Map (Name → Capacity)
+    static void convertToMap(List<Bogie> list) {
+        Map<String, Integer> map =
+                list.stream()
+                        .collect(Collectors.toMap(b -> b.name, b -> b.capacity));
+
+        System.out.println("\nBogie Map: " + map);
+    }
+
+    // UC20 - Skip & Limit
+    static void skipAndLimit(List<Bogie> list) {
+        System.out.println("\nSkip 1, Limit 2:");
+
+        list.stream()
+                .skip(1)
+                .limit(2)
+                .forEach(b -> System.out.println(b.name + " - " + b.capacity));
+    }
 }
